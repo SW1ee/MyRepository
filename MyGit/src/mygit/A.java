91dcec2a4da698245fdd7bbfa50023172a98e210
slesw <slesw@DESKTOP-JDA6I65>
2023-12-12 16:48:41 +0900

    First commit in dev branch

--- MyGit/src/mygit/A.java
+++ MyGit/src/mygit/A.java
@@ -3,5 +3,7 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("the 2nd version.");
+		System.out.println("First commit in dev branch");
 	}
 }
